d69a3005cc83d5fbfe56f78b22f456c472ad0459
NezitX <xnezit@gmail.com>
2024-12-29 18:08:30 +0300

    updates

--- readme.md
+++ readme.md
@@ -1,9 +1,16 @@
 ![image](https://raw.githubusercontent.com/NezitX/acode-better-github-dark/refs/heads/main/assets/better-githubdark-logo.png)
 
-Better GitHub Dark is a [Acode Plugin](https://acode.app/plugin-docs/) theme based on [Ace](https://ace.c9.io/) GitHub Dark theme.
+![GitHub Repo Shield](https://img.shields.io/github/repo-size/NezitX/acode-better-github-dark?color=pink&label=Size&style=for-the-badge)
+![License](https://img.shields.io/github/license/NezitX/acode-better-github-dark?color=pink&style=for-the-badge)
+![GitHub Stars](https://img.shields.io/github/stars/NezitX/acode-better-github-dark?color=pink&style=for-the-badge)
+![Version](https://img.shields.io/github/v/release/NezitX/acode-better-github-dark?color=pink&style=for-the-badge)
+
+<div align="center">
+  <p>Better GitHub Dark is a <a href="https://acode.app/">Acode</a> plugin theme based on <a href="https://ace.c9.io/">Ace</a> GitHub Dark theme.</p>
+</div>
 
 ## Preview
 ![image](https://raw.githubusercontent.com/NezitX/acode-better-github-dark/refs/heads/main/assets/example.png)
 
-## Note
-this project is fork from [acode editorTheme template](https://github.com/legendSabbir/acode-editorTheme-template/) thanks for the template.
+## License
+this plugin is under [MIT LICENSE](./LICENSE)
--- src/main.js
+++ src/main.js
@@ -3,88 +3,121 @@ import style from "./style.scss";
 
 const settings = acode.require("settings");
 const { editor } = editorManager;
+const themesList = ace.require("ace/ext/themelist").themes;
 
 const THEME_NAME = "better-github-dark";
 const THEME_IS_DARK = true;
 
+// Define theme CSS first
 ace.define(
   `ace/theme/${THEME_NAME}.css`,
   ["require", "exports", "module"],
-  function (require, exports, module) {
+  (require, exports, module) => {
     module.exports = style;
   }
 );
 
+// Simplified theme definition
 ace.define(
   `ace/theme/${THEME_NAME}`,
-  [
-    "require",
-    "exports",
-    "module",
-    `ace/theme/${THEME_NAME}.css`,
-    "ace/lib/dom"
-  ],
-  function (require, exports, module) {
+  ["require", "exports", "module", "ace/lib/dom"],
+  (require, exports, module) => {
+    const dom = require("ace/lib/dom");
+    
     exports.isDark = THEME_IS_DARK;
     exports.cssClass = `ace-${THEME_NAME}`;
-    exports.cssText = require(`./${THEME_NAME}.css`);
-    const dom = require("../lib/dom");
-    dom.importCssString(exports.cssText, exports.cssClass, false);
+    exports.cssText = style;
+    
+    // Use setTimeout to defer DOM manipulation
+    setTimeout(() => {
+      dom.importCssString(exports.cssText, exports.cssClass, false);
+    }, 0);
   }
 );
 
-(function () {
-  window.require([`ace/theme/${THEME_NAME}`], function (m) {
-    if (typeof module == "object" && typeof exports == "object" && module) {
-      module.exports = m;
-    }
-  });
-})();
-
 class BetterGithubDark {
-  async init() {
-    try {
-      ace.require("ace/ext/themelist").themes.push({
-        caption: THEME_NAME.split("-")
-          .map(name => name[0].toUpperCase() + name.slice(1))
-          .join(" "),
-        theme: "ace/theme/" + THEME_NAME,
-        isDark: THEME_IS_DARK
-      });
+  constructor() {
+    this.onThemeChange = this.onThemeChange.bind(this);
+    this.initialized = false;
+  }
 
+  async init($page, cacheFile, cacheFileUrl) {
+    if (this.initialized) return;
+    
+    try {
+      // Add theme to the list
+      const themeCaption = THEME_NAME.split("-")
+        .map(name => name[0].toUpperCase() + name.slice(1))
+        .join(" ");
+
+      const themeEntry = {
+        caption: themeCaption,
+        theme: `ace/theme/${THEME_NAME}`,
+        isDark: THEME_IS_DARK
+      };
+
+      if (!themesList.some(theme => theme.theme === themeEntry.theme)) {
+        themesList.push(themeEntry);
+      }
+
+      // Set theme if it's currently selected
       const currentTheme = settings.get("editorTheme");
-      if (currentTheme === THEME_NAME)
-        editor.setTheme("ace/theme/" + THEME_NAME);
-      settings.on("update", this.onThemeChange.bind(this));
+      if (currentTheme === THEME_NAME) {
+        // Defer theme setting
+        setTimeout(() => {
+          editor.setTheme(`ace/theme/${THEME_NAME}`);
+        }, 0);
+      }
+
+      // Add event listener
+      settings.on("update", this.onThemeChange);
+      this.initialized = true;
     } catch (error) {
-      console.error(error);
+      console.error("Theme initialization error:", error);
       acode.alert("Warning", "Please restart acode");
     }
   }
 
   async destroy() {
+    if (!this.initialized) return;
+    
     settings.off("update", this.onThemeChange);
+    
+    const themeIndex = themesList.findIndex(
+      t => t.theme === `ace/theme/${THEME_NAME}`
+    );
+    
+    if (themeIndex !== -1) {
+      themesList.splice(themeIndex, 1);
+    }
+    
+    this.initialized = false;
   }
 
   onThemeChange(value) {
     if (typeof value !== "string") return;
+    
     let themeName = value;
-    if (value.startsWith("ace/theme/"))
-      themeName = value?.replace("ace/theme/", "");
+    if (value.startsWith("ace/theme/")) {
+      themeName = value.replace("ace/theme/", "");
+    }
 
-    editor.setTheme(`ace/theme/${themeName}`);
-    settings.update({ editorTheme: themeName });
+    // Defer theme setting
+    setTimeout(() => {
+      editor.setTheme(`ace/theme/${themeName}`);
+      settings.update({ editorTheme: themeName });
+    }, 0);
   }
 }
 
+// Plugin initialization
 if (window.acode) {
   const betterGithubDark = new BetterGithubDark();
+  
   acode.setPluginInit(
     plugin.id,
     async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
-      if (!baseUrl.endsWith("/")) baseUrl += "/";
-
-      betterGithubDark.baseUrl = baseUrl;
+      betterGithubDark.baseUrl = baseUrl.endsWith("/") ? baseUrl : baseUrl + "/";
       await betterGithubDark.init($page, cacheFile, cacheFileUrl);
     }
   );
